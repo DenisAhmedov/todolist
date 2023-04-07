045c6bd06fba11b8d4153b23991a0a150abd36cb
Denis Akhmedov <denis-dag@mail.ru>
2023-04-08 01:43:55 +0300

    add board models insite.admin

--- src/goals/admin.py
+++ src/goals/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory, Goal, GoalComment
+from goals.models import GoalCategory, Goal, GoalComment, Board
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'is_deleted')
+    list_display = ('id', 'title', 'user', 'is_deleted', 'board')
     search_fields = ('title',)
     list_display_links = ('title',)
     list_filter = ('is_deleted',)
@@ -26,4 +26,13 @@ class GoalCommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'text')
     list_display_links = ('text',)
     search_fields = ('text',)
+    readonly_fields = ('created', 'updated')
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_deleted')
+    search_fields = ('title',)
+    list_display_links = ('title',)
+    list_filter = ('is_deleted', )
     readonly_fields = ('created', 'updated')
